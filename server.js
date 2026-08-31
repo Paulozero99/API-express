aa9c3f4b35406898ae1ed35e6151ca4a4f62601c
Paulozero99 <opaulohenrique363@gmail.com>
2026-08-31 17:31:34 -0400

    Configurando as rotas do server no modelo da framework express

--- server.js
+++ server.js
@@ -3,8 +3,40 @@ import express from 'express'
 const app = express()
 const PORTA = 3000
 
+let usuarios = [
+    {id: 1, nome: 'Gabriel'},
+    {id: 2, nome: 'João'},
+    {id: 3, nome: 'Paulo'}
+]
+
 app.get('/', (req, res) => {
     res.send(`Servidor Express funcionando!`)
+})
+
+app.get('/usuarios', (req, res) => {
+    res.json(usuarios)
+})
+
+app.post('/usuarios', (req, res) => {
+    const novoUsuario = {
+        id: usuarios.length + 1,
+        nome: "lucas"
+    }
+
+    usuarios.push(novoUsuario)
+    res.status(201).json(novoUsuario)
+})
+
+app.get('usuario/:id', (req, res) => {
+    const id = req.params.id
+
+    const usuario = usuarios.find(u => u.id === parseInt(id))
+
+    if(!usuario){
+        return res.status(404).json({error: "Usuario não encontrado!"})
+    }
+
+    res.json(usuario)
 })
 
 app.listen(PORTA, () => {
